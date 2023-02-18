8a6822c3639c9905cc3dad1383fe48bc901a388f
adrian-burkhart <adrian.burkhart@gmail.com>
2023-02-18 15:56:23 +0100

    Implement sorting

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,55 @@ export const App = () => {
     players[0]
   )
 
+  const [sortedPlayers, setSortedPlayers] = React.useState<Player[]>([])
+
+  const [sortMethod, setSortMethod] = React.useState<
+    'asc' | 'desc' | 'unsorted'
+  >('unsorted')
+
+  const handleClick = React.useCallback(
+    (method: 'asc' | 'desc') => {
+      if (method === sortMethod) {
+        setSortMethod('unsorted')
+      } else {
+        setSortMethod(method)
+      }
+    },
+    [sortMethod]
+  )
+
+  React.useEffect(() => {
+    if (sortMethod === 'asc') {
+      sortPlayers('asc')
+    } else if (sortMethod === 'desc') {
+      sortPlayers('desc')
+    } else {
+      setSortedPlayers(players)
+    }
+  }, [players, sortMethod])
+
+  const sortPlayers = React.useCallback(
+    (direction: 'asc' | 'desc') => {
+      const sortedPlayers = [...players].sort((a, b) => {
+        if (direction === 'asc') {
+          return a.playerName.localeCompare(b.realName)
+        } else {
+          return b.playerName.localeCompare(a.realName)
+        }
+      })
+      setSortedPlayers(sortedPlayers)
+    },
+    [players]
+  )
+
   return (
     <main>
       <Heading level={1}>Demo Card Viewer</Heading>
       <div className='grid grid-cols-2 gap-6 my-20'>
         <DetailView player={activePlayer} />
-        <SettingsCard loadingState={loadingState} />
+        <SettingsCard loadingState={loadingState} handleClick={handleClick} />
         <PlayerOverview
-          players={players}
+          players={sortedPlayers}
           loadingState={loadingState}
           setActivePlayer={setActivePlayer}
           activePlayer={activePlayer}
--- src/components/settings-card.tsx
+++ src/components/settings-card.tsx
@@ -1,21 +1,33 @@
 import { LoadingState } from '../hooks'
-import { Button, Heading, Card } from '../ui'
+import { Button, Heading, Card, Text } from '../ui'
 
 interface SettingsCardProps {
   loadingState: LoadingState
+  handleClick: (method: 'asc' | 'desc') => void
 }
 
-export const SettingsCard = ({ loadingState }: SettingsCardProps) => {
+export const SettingsCard = ({
+  loadingState,
+  handleClick,
+}: SettingsCardProps) => {
   return (
     <div className='flex flex-col'>
       <Heading level={2}>Controls</Heading>
       <Card big>
         <div className='grid grid-cols-2 gap-2 my-4 '>
           <div>
-            <Button inactive={loadingState !== 'success'} label='Sort Asc' />
+            <Button
+              inactive={loadingState !== 'success'}
+              label='Sort Asc'
+              onClick={() => handleClick('asc')}
+            />
           </div>
           <div>
-            <Button inactive={loadingState !== 'success'} label='Sort Desc' />
+            <Button
+              inactive={loadingState !== 'success'}
+              label='Sort Desc'
+              onClick={() => handleClick('desc')}
+            />
           </div>
           <div className='col-span-2'>
             <Button inactive={loadingState !== 'success'} label='Submit' />
