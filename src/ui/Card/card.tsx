6d0db7836a40091a2ae48f371d490d77cf24b822
adrian-burkhart <adrian.burkhart@gmail.com>
2023-02-18 16:41:08 +0100

    Fix hover bug

--- src/ui/Card/card.tsx
+++ src/ui/Card/card.tsx
@@ -19,9 +19,10 @@ export const Card = ({
     <div
       className={classNames(
         'bg-white py-3 px-1 h-[10rem] rounded-md transition-all duration-200 flex flex-col justify-center',
-        highlighted ? 'shadow-inner hover:mb-0 hover:mt-0' : 'shadow-test',
         clickable &&
+          !highlighted &&
           'active:shadow-inner hover:mb-1 hover:-mt-1 active:mb-0 active:mt-0',
+        highlighted ? 'shadow-inner hover:mb-0 hover:-mt-0' : 'shadow-test',
         big && 'min-h-[15rem]'
       )}
       onClick={clickable ? onClick : undefined}
