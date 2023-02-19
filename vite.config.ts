5ed3d49355ca4d7cffdac6383cb2a226d6c90491
adrian-burkhart <adrian.burkhart@gmail.com>
2023-02-19 19:18:44 +0100

    Configure vitest with jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    exclude: [...configDefaults.exclude, 'packages/template/*'],
+    environment: 'jsdom',
+    globals: true,
+  },
 })
