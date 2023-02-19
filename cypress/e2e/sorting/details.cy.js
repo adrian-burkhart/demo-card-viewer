d6288e98d407e476c922c0546512ae2c493c8c65
adrian-burkhart <adrian.burkhart@gmail.com>
2023-02-19 19:02:10 +0100

    Finish e2e test

--- cypress/e2e/sorting/details.cy.js
+++ cypress/e2e/sorting/details.cy.js
@@ -2,14 +2,11 @@
 
 describe('Demo Card Viewer', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5173/')
+    cy.visit('http://127.0.0.1:5173/') // change URL to match your dev URL
   })
 
-  it('can display player details', () => {
+  it('can display player details when a card is clicked', () => {
     cy.get('[data-cy=player-overview-card]').first().click()
-    cy.get('[data-cy=detail-view]').should('contain', "Last Name")
+    cy.get('[data-cy=detail-view]').should('contain', 'Last Name')
   })
-
-
-
 })
